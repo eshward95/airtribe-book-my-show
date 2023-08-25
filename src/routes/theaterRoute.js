5b14b6ac96989b5421187997f020145b4f76ce4f
Eshwar <eshwar.devaramane@yellow.ai>
2023-08-25 18:33:32 +0530

    chore:Minor theater router changes

--- src/routes/theaterRoute.js
+++ src/routes/theaterRoute.js
@@ -3,13 +3,19 @@ const express = require("express");
 const {
   getTheater,
   getShow,
+  bookTicket,
   getSeat,
+  //   getMovie,
 } = require("../controllers/theaterController");
 
 const router = express.Router();
-
+//Get theater details
 router.get("/:name", getTheater);
+//Get shows in theater
 router.get("/:name/show", getShow);
-router.get("/:name/seat", getSeat);
+//Get movie in details
+router.get("/:name/movie", getShow);
+// router.get("/:name/seat", getSeat);
+router.post("/:name/:show_time_id/book", bookTicket);
 
 module.exports = router;
